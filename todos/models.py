b325489a2a5fec0d7ec19dabf9628e8330e3743b
Hieu Ho <hieu_ho@helloleotech.com>
2021-10-09 14:39:53 -0700

    Implement todos list crud operations

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,13 @@
+from helpers.models import TrackingModel
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+
+class Todo(TrackingModel):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    is_complete = models.BooleanField(default=False)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
