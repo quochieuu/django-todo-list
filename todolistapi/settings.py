006c64f102f9232a994b7d5956e83cfd16571857
Hieu Ho <hieu_ho@helloleotech.com>
2021-10-10 13:23:52 -0700

    Implement swagger

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'authentication',
     'django_filters',
     'todos',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,17 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': "rest_framework.pagination.PageNumberPagination",
     'PAGE_SIZE': 12
 
+}
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
 }
 
 
